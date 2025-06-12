3b2421543e3d3df9310f44ae905b1ed850abde7e
Dhaval Patel <dhava.p@uplers.in>
2025-06-12 17:02:54 +0530

    Code updated

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -4,6 +4,8 @@ import { getTestimonialsData } from '../about-us/server/testimonials-data';
 import './contact-style.css';
 import { gql } from "@apollo/client";
 import { client } from "../lib/apollo-client";
+import { ContactForm } from "./contactform";
+import MapComponent from "../components/mapcomponents";
 
 interface SocialMedia {
     image?: {
@@ -22,6 +24,8 @@ const GET_CONTACT_PAGE_DATA = gql`
 query ContactPage {
     page(id: "contact-us", idType: URI) {
         contactUs {
+        formTitle
+      formContent
             map
             address {
                 addressText
@@ -77,78 +81,16 @@ export default async function ContactUs() {
                     <div className="flex flex-wrap">
                         <div className="w-full lg:w-3/5 contact-form pb-12 lg:pb-0">
                             <div className="bg-gradient-to-b from-[#E9EFE9] rounded-3xl py-6 px-4 lg:p-12 pb-12 md:pb-0">
-                                <h1 className="h2 text-center mb-6">
-                                    We&apos;d <span role="img" aria-label="love">💖</span> love to hear from you
-                                </h1>
-                                <p className="text-base md:text-1xl text-center mb-8">
-                                    Want to take your email marketing to the next level?
-                                </p>
-                                <form className="space-y-6">
-                                    <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6">
-                                        <input
-                                            type="text"
-                                            placeholder="Your name*"
-                                            required
-                                            className="rounded-lg px-4 py-3  col-span-1" />
-                                        <input
-                                            type="email"
-                                            placeholder="Email address*"
-                                            required
-                                            className="rounded-lg px-4 py-3  col-span-1" />
-                                        <input
-                                            type="tel"
-                                            placeholder="Phone number*"
-                                            required
-                                            className="rounded-lg px-4 py-3  col-span-1" />
-                                        <input
-                                            type="text"
-                                            placeholder="Website*"
-                                            required
-                                            className="rounded-lg px-4 py-3  col-span-1" />
-                                    </div>
-                                    <select
-                                        required
-                                        className="rounded-lg px-4 py-3 bg-theme-light-gray 
-                    focus:outline-none focus:ring-2 focus:ring-theme-blue w-full mb-4"
-                                    >
-                                        <option value="">Services Interested*</option>
-                                        <option>Email Marketing Strategy</option>
-                                        <option>Template Design & Coding</option>
-                                        <option>Campaign Management</option>
-                                        <option>Automation & Workflows</option>
-                                        <option>Other</option>
-                                    </select>
-                                    <textarea
-                                        placeholder="Type your masseege..."
-                                        rows={4}
-                                        className="rounded-lg px-4 py-3 bg-theme-light-gray 
-                    focus:outline-none focus:ring-2 focus:ring-theme-blue w-full mb-4 resize-none"
-                                    />
-                                    <div className="flex items-center mb-6">
-                                        <input
-                                            type="checkbox"
-                                            id="newsletter"
-                                            className="accent-theme-blue w-5 h-5 mr-2"
-                                            defaultChecked
-                                        />
-                                        <label htmlFor="newsletter" className="text-theme-text-2 text-base select-none">
-                                            Subscribe to our Newsletter
-                                        </label>
-                                    </div>
-                                    <button type="submit" className="w-full bg-theme-blue text-white font-semibold py-3 rounded-lg text-lg  transition hover:bg-theme-d font-intersemi border-none"> Send A Request </button>
-                                    <p className="text-sm italic text-theme-text-2 text-center mt-4">
-                                        (InboxArmy doesn&apos;t work or provide email list buying or rental service.)
-                                    </p>
-                                </form>
+                                {contactData.formTitle && <h1 className="h2 text-center mb-6" dangerouslySetInnerHTML={{ __html: contactData.formTitle }}></h1>}
+                                {contactData.formContent && <div className="text-base md:text-1xl text-center mb-8" dangerouslySetInnerHTML={{ __html: contactData.formContent }}></div>}
+                                <ContactForm />
                             </div>
                         </div>
                         {/* Contact Info & Social Links Section */}
                         {contactData && (
                             <div className="w-full lg:w-2/5 lg:pl-20">
                                 {/* Map */}
-                                {contactData.map && (
-                                    <div className="rounded-2xl overflow-hidden mb-6 map-wrap" dangerouslySetInnerHTML={{ __html: contactData.map }}></div>
-                                )}
+                                <MapComponent mapContent={contactData.map} />
                                 <div className="space-y-6 lg:space-y-8 px-4 lg:px:0">
                                     {contactData.address && (
                                         <div className="flex items-start gap-3">
